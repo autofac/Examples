a99c35cde094eda0f08870e44f284b8746df06ae
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-04 22:42:34 +1000

    Moved projects to src folder.

---
+++ src/Remember.Model/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace Remember.Model
+{
+    public class Task : IIdentifiable
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Title { get; set; }
+
+        public virtual bool IsComplete { get; set; }
+    }
+}
---
+++ src/Remember.Web/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Remember.Web.Models;
+
+namespace Remember.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("LoginSuccess");
+            }
+
+            
+            return View(model);
+        }
+
+        
+        public ActionResult LoginSuccess()
+        {
+            return View();
+        }
+    }
+}
