7e3563ab9832f78b4ddb8dee01a9a60f50fdf396
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2017-04-11 22:23:46 +1000

    Removed Service Fabric demo from AppVeyor build

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
 msbuild .\Examples.sln /t:Rebuild /verbosity:Minimal /p:Configuration=Release
-msbuild .\src\ServiceFabricDemo\AutofacServiceFabricDemo.sln /t:Rebuild /verbosity:Minimal /p:Configuration=Release /p:Platform=x64
+if ($env:APPVEYOR_BUILD_NUMBER -eq $NULL) {
+	msbuild .\src\ServiceFabricDemo\AutofacServiceFabricDemo.sln /t:Rebuild /verbosity:Minimal /p:Configuration=Release /p:Platform=x64
+}
 .\build-aspnetcore.ps1
