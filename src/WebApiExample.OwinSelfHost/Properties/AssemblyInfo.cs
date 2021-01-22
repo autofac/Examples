6dd04fd086fcefc8c8227d9113049353c521d699
Travis Illig <tillig@paraesthesia.com>
2021-01-22 13:53:07 -0800

    Switch WebAPI self host EXE to .NET Core project structure.

--- src/WebApiExample.OwinSelfHost/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("WebApiExample.OwinSelfHost")]
-[assembly: AssemblyProduct("WebApiExample.OwinSelfHost")]
-[assembly: AssemblyVersion("1.0.0.0")]
