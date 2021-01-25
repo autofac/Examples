bb02840d4c66cc5c717c56019ec3617728c4ca69
Travis Illig <tillig@paraesthesia.com>
2021-01-25 07:37:48 -0800

    Enabling multi-platform build.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,5 @@
-nuget restore .\Examples.sln
-msbuild .\Examples.sln /t:Rebuild /verbosity:Minimal /p:Configuration=Release
-if ($env:APPVEYOR_BUILD_NUMBER -eq $NULL) {
+dotnet build .\Examples.sln
+if ($NULL -eq $env:APPVEYOR_BUILD_NUMBER -and $env:APPVEYOR_BUILD_WORKER_IMAGE -like "Visual Studio*") {
   nuget restore .\src\ServiceFabricDemo\AutofacServiceFabricDemo.sln
   msbuild .\src\ServiceFabricDemo\AutofacServiceFabricDemo.sln /t:Rebuild /verbosity:Minimal /p:Configuration=Release /p:Platform=x64
 }
