1c84f81ba7c495689922c6b31e3a3eef2406eb0a
Travis Illig <tillig@paraesthesia.com>
2021-01-25 07:41:27 -0800

    Minor clarification on building ServiceFabric.

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 dotnet build .\Examples.sln
-if ($NULL -eq $env:APPVEYOR_BUILD_NUMBER -and $env:APPVEYOR_BUILD_WORKER_IMAGE -like "Visual Studio*") {
+if ($NULL -eq $env:APPVEYOR_BUILD_NUMBER -and $IsWindows) {
+  # Only build the Service Fabric demo when it's not AppVeyor and the current environment is Windows.
   nuget restore .\src\ServiceFabricDemo\AutofacServiceFabricDemo.sln
   msbuild .\src\ServiceFabricDemo\AutofacServiceFabricDemo.sln /t:Rebuild /verbosity:Minimal /p:Configuration=Release /p:Platform=x64
 }
