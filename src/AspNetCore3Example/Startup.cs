691e302ef8fe73bdbceb39445607bfb3b1500b31
Travis Illig <tillig@paraesthesia.com>
2021-01-25 07:51:45 -0800

    Show how diagnostics can be used in ASP.NET Core.

--- src/AspNetCore3Example/Startup.cs
+++ src/AspNetCore3Example/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Autofac;
+﻿using System;
+using Autofac;
+using Autofac.Diagnostics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,6 +25,25 @@ namespace AspNetCore3Example
             // You must have the call to `UseServiceProviderFactory(new AutofacServiceProviderFactory())`
             // when building the host or this won't be called.
             builder.RegisterModule(new AutofacModule());
+
+            // If you want to enable diagnostics, you can do that via a build
+            // callback. Diagnostics aren't free, so you shouldn't just do this
+            // by default. Note: since you're diagnosing the container you can't
+            // ALSO resolve the logger to which the diagnostics get written, so
+            // writing directly to the log destination is the way to go.
+            /*
+            var tracer = new DefaultDiagnosticTracer();
+            tracer.OperationCompleted += (sender, args) =>
+            {
+                Console.WriteLine(args.TraceContent);
+            };
+
+            builder.RegisterBuildCallback(c =>
+            {
+                var container = c as IContainer;
+                container.SubscribeToDiagnostics(tracer);
+            });
+            */
         }
 
         public void ConfigureServices(IServiceCollection services)
