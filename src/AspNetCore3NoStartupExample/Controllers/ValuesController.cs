7406e2e175c308be2441f4b9f3d03d7d0494a205
Travis Illig <tillig@paraesthesia.com>
2021-02-11 07:05:53 -0800

    Example showing ASP.NET Core without a Startup class.

---
+++ src/AspNetCore3NoStartupExample/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using AspNetCore3NoStartupExample.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore3NoStartupExample.Controllers
+{
+    /// <summary>
+    /// Simple REST API controller that shows Autofac injecting dependencies.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IValuesService _valuesService;
+
+        public ValuesController(IValuesService valuesService)
+        {
+            this._valuesService = valuesService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return this._valuesService.FindAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return this._valuesService.Find(id);
+        }
+    }
+}
---
+++ src/AspNetCore3NoStartupExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore3NoStartupExample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // The example here is exactly the same as AspNetCore3Example, but this
+            // shows how you'd use the AutofacServiceProviderFactory and container
+            // configuration when you don't have a Startup class. The
+            // GenericHostBuilderExample shows how you do this when it's generic
+            // hosted services and not ASP.NET Core.
+            var host = Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webHostBuilder =>
+                {
+                    webHostBuilder.Configure(app =>
+                    {
+                        app.UseRouting();
+                        app.UseEndpoints(builder => builder.MapControllers());
+                    });
+
+                    webHostBuilder.ConfigureServices(services => services.AddControllers());
+                })
+                .ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacModule()))
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
